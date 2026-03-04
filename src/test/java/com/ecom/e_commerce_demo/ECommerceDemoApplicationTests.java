5faf711e6dd902511ef19bcbfff792896d0a9f10
AkshayThokaleGithub <akshaythokale@gmail.com>
2026-03-04 12:07:36 +0530

    code modified

--- src/test/java/com/ecom/e_commerce_demo/ECommerceDemoApplicationTests.java
+++ src/test/java/com/ecom/e_commerce_demo/ECommerceDemoApplicationTests.java
@@ -3,10 +3,12 @@ package com.ecom.e_commerce_demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
+@AutoConfigureMockMvc
 class ECommerceDemoApplicationTests {
     @Autowired
 	private MockMvc mockMvc;
@@ -14,8 +16,8 @@ class ECommerceDemoApplicationTests {
 	@Test
 	void testGreetingEndpoint() throws Exception {
 		String name= "Shubhangi Akshay Thokale";
-   mockMvc.perform(MockMvcRequestBuilders.get("greetings/{name}", name)).andExpect(MockMvcResultMatchers.status().isOk())
-   .andExpect(MockMvcResultMatchers.content().string("Hello "+name+" Congratulation you have successfully completed the devops course."));
+   mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", name)).andExpect(MockMvcResultMatchers.status().isOk())
+   .andExpect(MockMvcResultMatchers.content().string("Hello "+name+" 	Congratulation you have successfully completed the devops course."));
    
 		;
 	}
